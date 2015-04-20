7426d1eea7ad548b3e2326ce1d2186e257d92aca
Christoph Rissner <resi@rissner.net>
2015-04-20 22:50:31 +0200

    inital commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = "sanesane",
+    version="0.0.1",
+    author = "Christoph Rissner",
+    author_email = "cri@flinkwork.at",
+    description = "Sane sane interface to scan stuff.",
+    url = "http://www.flinkwork.com",
+
+    install_requires = ["pillow", "pypdf2", "pyinsane"],
+
+    entry_points = {"console_scripts":
+                    ["sanesane = sanesane:main",
+                    ],
+                   }
+)
